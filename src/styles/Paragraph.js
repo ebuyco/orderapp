533576bcbc94951164b2dfa13bb219374c9bfa85
ebuyco <buycoernie@gmail.com>
2019-01-30 19:07:41 +0800

    added_styles

---
+++ src/styles/Paragraph.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Paragraph = styled.p`
+    color: red;
+    font-family: Montserrat-Regular;
+    font-size: 3rem;
+    font-weight: bold;
+    margin-top: 8rem;
+    text-align: center;
+`;
+
+export default Paragraph;
